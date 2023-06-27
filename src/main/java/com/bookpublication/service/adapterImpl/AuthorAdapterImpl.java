1fbc9b5c24099e378fe38770c987a09df2f1f3fe
Hirush Gimhan <hirushgimhan2016@gmail.com>
2023-06-28 01:29:35 +0530

    Added logger levels

--- src/main/java/com/bookpublication/service/adapterImpl/AuthorAdapterImpl.java
+++ src/main/java/com/bookpublication/service/adapterImpl/AuthorAdapterImpl.java
@@ -7,16 +7,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 /**
  * This class is the AuthorAdapterImpl class
  * It is used to implement CRUD operations on the Author entity
- *
  */
 
 @Component
 public class AuthorAdapterImpl implements AuthorAdapter {
 
+    //Initialize logger
+    private static final Logger logger = Logger.getLogger(AuthorAdapterImpl.class.getName());
     public final AuthorRepository authorRepository;
 
     @Autowired
@@ -29,6 +31,7 @@ public class AuthorAdapterImpl implements AuthorAdapter {
 
         //Check if author is null
         if (author == null) {
+            logger.severe("Author should not be null");
             return "Author should not be null";
         } else {
 
@@ -41,13 +44,16 @@ public class AuthorAdapterImpl implements AuthorAdapter {
                 //Save author
                 authorRepository.save(author);
             } catch (Exception e) {
+                logger.severe("Author not saved : " + e.getMessage());
                 return "Author not saved : " + e.getMessage();
             }
 
             //check if author is saved
             if (authorRepository.existsById(author.getId())) {
+                logger.info("Author saved successfully");
                 return "Author saved successfully";
             } else {
+                logger.severe("Author not saved");
                 return "Author not saved";
             }
 
@@ -59,6 +65,7 @@ public class AuthorAdapterImpl implements AuthorAdapter {
     public Author getAuthor(Long id) {
         //Check if author exists
         if (authorRepository.existsById(id)) {
+            logger.info("Author found");
             return authorRepository.findById(id).get();
         } else {
             return null;
@@ -69,6 +76,7 @@ public class AuthorAdapterImpl implements AuthorAdapter {
     public String updateAuthorById(Long id, Author author) {
         //Check if author is null
         if (author == null) {
+            logger.severe("Author should not be null");
             return "Author should not be null";
         } else {
 
@@ -86,8 +94,10 @@ public class AuthorAdapterImpl implements AuthorAdapter {
                 authorToUpdate.setContactNumber(author.getContactNumber());
 
                 authorRepository.save(authorToUpdate);
+                logger.info("Author updated successfully");
 
             } catch (Exception e) {
+                logger.severe("Author not updated : " + e.getMessage());
                 return "Author not updated : " + e.getMessage();
             }
 
@@ -104,23 +114,22 @@ public class AuthorAdapterImpl implements AuthorAdapter {
             try {
                 //Delete author
                 authorRepository.deleteById(id);
+                logger.info("Author deleted successfully");
             } catch (Exception e) {
+                logger.severe("Author not deleted : " + e.getMessage());
                 return "Author not deleted : " + e.getMessage();
             }
 
-            //check if author is deleted
-            if (!authorRepository.existsById(id)) {
-                return "Author deleted successfully";
-            } else {
-                return "Author not deleted";
-            }
         } else {
+            logger.severe("Author not found");
             return "Author not found";
         }
+        return "Author not found";
     }
 
     @Override
     public List<Author> getAllAuthors() {
+        logger.info("Authors found");
         return authorRepository.findAll();
     }
 }
