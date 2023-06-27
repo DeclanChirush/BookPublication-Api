285aaeec9063591a7c53544c168e082b568ef543
Hirush Gimhan <hirushgimhan2016@gmail.com>
2023-06-28 01:35:41 +0530

    Book model created

---
+++ src/main/java/com/bookpublication/dal/model/Book.java
@@ -0,0 +1,38 @@
+package com.bookpublication.dal.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * This class is used to represent the Book entity.
+ * It is used to map the Book entity to the books table in the database.
+ *
+ */
+@Entity
+@Table(name = "books")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String isbn;
+
+    private String category;
+
+    private String title;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    @JsonIgnore // Ignore the author field during serialization
+    private Author author;
+
+}
