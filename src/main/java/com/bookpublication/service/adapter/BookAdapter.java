9e9aeffc2494329f189bbc416aea78b5a1b5a118
Hirush Gimhan <hirushgimhan2016@gmail.com>
2023-06-28 01:35:55 +0530

    Book adapter created

---
+++ src/main/java/com/bookpublication/service/adapter/BookAdapter.java
@@ -0,0 +1,20 @@
+package com.bookpublication.service.adapter;
+
+import com.bookpublication.dal.model.Book;
+import com.bookpublication.dto.BookDto;
+
+import java.util.List;
+
+/**
+ * This class is the BookAdapter class
+ * It is used to perform CRUD operations on the Book entity
+ *
+ */
+
+public interface BookAdapter {
+     String saveBook(BookDto bookDto);
+     Book getBook(Long id);
+     String updateBookById(Long id, BookDto bookDto);
+     String deleteBook(Long id);
+     List<Book> getAllBooks();
+}
