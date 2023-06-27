0196a9db3c8e92dbdd6ae6db50249f15c2965418
Hirush Gimhan <hirushgimhan2016@gmail.com>
2023-06-28 01:53:15 +0530

    Removed unnecessary code lines

--- src/main/java/com/bookpublication/dal/model/Book.java
+++ src/main/java/com/bookpublication/dal/model/Book.java
@@ -6,9 +6,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * This class is used to represent the Book entity.
  * It is used to map the Book entity to the books table in the database.
