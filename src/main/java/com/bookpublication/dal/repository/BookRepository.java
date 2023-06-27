f66ba83dd6b8097513513b460e04d112438a0230
Hirush Gimhan <hirushgimhan2016@gmail.com>
2023-06-28 01:36:33 +0530

    Book repository created

---
+++ src/main/java/com/bookpublication/dal/repository/BookRepository.java
@@ -0,0 +1,14 @@
+package com.bookpublication.dal.repository;
+
+import com.bookpublication.dal.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * This class is the BookRepository class
+ * It is used to perform CRUD operations on the Book entity
+ *
+ */
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
