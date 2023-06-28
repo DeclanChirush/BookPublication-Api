8a9020fbb633d1aedf74cad3a127b992fd3038e4
Hirush Gimhan <hirushgimhan2016@gmail.com>
2023-06-28 10:42:45 +0530

    Added new findByAuthorId method

--- src/main/java/com/bookpublication/dal/repository/BookRepository.java
+++ src/main/java/com/bookpublication/dal/repository/BookRepository.java
@@ -4,6 +4,8 @@ import com.bookpublication.dal.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * This class is the BookRepository class
  * It is used to perform CRUD operations on the Book entity
@@ -14,4 +16,5 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     boolean existsByIsbn(String isbn);
     Book findByIsbn(String isbn);
     void deleteByAuthorId(Long id);
+    List<Book> findByAuthorId(Long id);
 }
